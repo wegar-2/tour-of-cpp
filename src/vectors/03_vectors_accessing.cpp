28ad3b5300aa05f0fd9ec56564bd31ac5236b36a
wegar2 <awegrzyn17@gmail.com>
2026-09-23 19:31:24 +0200

    completed note on accessing vectors for writing and reading

--- src/vectors/03_vectors_accessing.cpp
+++ src/vectors/03_vectors_accessing.cpp
@@ -27,13 +27,13 @@ void print_vec(vector<int> vec, bool multiline = true) {
 
 
 void double_vec_in_place(vector<int>& vec) {
-    for (auto el : vec) { el *= 2; }
+    for (auto& el : vec) { el *= 2; }
 }
 
 
 vector<int> double_vec(const vector<int>& vec) {
     vector<int> out(vec.size());
-    for (size_t i = 0; i < vec.size(); ++i) { out[i] = vec[i]; }
+    for (size_t i = 0; i < vec.size(); ++i) { out[i] = 2 * vec[i]; }
     return out;
 }
 
@@ -42,14 +42,17 @@ vector<int> double_vec(const vector<int>& vec) {
 int main() {
     vector<int> vec = {1, 323, 19, 3, 8, 23};
 
-    cout << "Printing vec in multiple line..." << endl;
+    cout << "STEP 1: Printing vec in multiple line..." << endl;
     print_vec(vec, true);
 
-    cout << "Printing vec in single line..." << endl;
+    cout << "STEP 2: Printing vec in single line..." << endl;
     print_vec(vec, false);
-    
-    //cout << "Accessing with begin: " << endl;
-    // cout << "vec.begin() = " << vec.begin() << endl;
-    //cout << "vec.end() = " << vec.end() << endl;
-    //cout << vec[vec.begin()] << endl;
+
+    cout << "STEP 3: doubling the vector in place..." << endl;
+    double_vec_in_place(vec);
+    print_vec(vec, false);
+
+    cout << "STEP 4: doubling and returning copy..." << endl;
+    vector<int> vec_copy = double_vec(vec);
+    print_vec(vec_copy, false);
 }
