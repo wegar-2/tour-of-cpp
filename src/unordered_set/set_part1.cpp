4516278c2fd452c28a4a62346abe5ec542df3331
wegar2 <awegrzyn17@gmail.com>
2026-09-25 22:03:12 +0200

    basics of unordered_set API

--- src/unordered_set/set_part1.cpp
+++ src/unordered_set/set_part1.cpp
@@ -1,4 +1,5 @@
 #include <set>
+#include<iostream>
 
 using namespace std;
 
@@ -14,10 +15,18 @@ int main() {
     s2.insert(1);
     s2.insert(23);
     s2.insert(333);
+    cout << "s2.size() = " << s2.size() << endl;
 
+    print_set_of_ints(s1);
+    print_set_of_ints(s2);
 }
 
 
-void print_set_of_ints(const set<int> s) {
-    
+void print_set_of_ints(const set<int>& s) {
+    cout << "Printing contents of a set: " << endl;
+    int counter = 0;
+    for (auto x : s) {
+        counter++;
+        cout << "Element #" << counter << " = " << x << endl;
+    }
 };
